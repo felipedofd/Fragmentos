812627d8a598918fc968bd5da26f71b8ffeaed00
felipedofd <felipedofd@hotmail.com>
2021-07-27 18:12:49 -0300

    start project

---
+++ app/src/main/java/br/com/fragmentos/Artigo1Fragment.java
@@ -0,0 +1,18 @@
+package br.com.fragmentos;
+
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+public class Artigo1Fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_1_artigo, container, false);
+    }
+}
--- app/src/main/java/br/com/fragmentos/MainActivity.java
+++ app/src/main/java/br/com/fragmentos/MainActivity.java
@@ -1,4 +1,26 @@
 package br.com.fragmentos;
 
-public class MainActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activty_main);
+
+        FragmentContainerView containerView = findViewById(R.id.container);
+
+        getSupportFragmentManager().beginTransaction().replace(containerView.getId(), new Artigo1Fragment() ,"Artigo_1").addToBackStack("Artigo_1").commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        getSupportFragmentManager().popBackStack();
+    }
 }
